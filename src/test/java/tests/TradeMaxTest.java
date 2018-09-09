0f8837d191f84a261b693e5072e5ad3b55eb26aa
vvs112 <twopa@yahoo.com>
2018-09-09 21:27:17 +0300

    commit__

--- src/test/java/tests/TradeMaxTest.java
+++ src/test/java/tests/TradeMaxTest.java
@@ -1,9 +1,10 @@
 package tests;
 
 
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import listeners.ReportGeneratorListener;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -17,20 +18,22 @@ import pages.ProductPage;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-@Listeners({ReportGeneratorListener.class})
+//@Listeners({ReportGeneratorListener.class})
 public class TradeMaxTest {
 
-    private HomePage homePage;
-    private ProductPage productPage;
-    private CheckOutPage checkOutPage;
-    private WebDriver driver;
+//    private HomePage homePage;
+//    private ProductPage productPage;
+//    private CheckOutPage checkOutPage;
+//    private WebDriver driver;
 
     @BeforeClass
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().fullscreen();
-        WebDriverRunner.setWebDriver(driver);
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().fullscreen();
+//        WebDriverRunner.setWebDriver(driver);
     }
 
     @AfterClass(alwaysRun = true)
@@ -41,32 +44,33 @@ public class TradeMaxTest {
 
     @Test
     public void searchForItemAndAddToCart(){
-        homePage = new HomePage();
-        productPage = homePage.searchForItem("525683");
-        productPage.addItemToCart();
+        Selenide.open("http://google.com");
+        Selenide.$(By.name("q")).val("selenide").pressEnter();
+        Selenide.$$("#ires li.g").shouldHave(CollectionCondition.size(0));
+        Selenide.$("#ires").find(By.linkText("selenide.org")).shouldBe(Condition.visible);
     }
 
-    @Test(dependsOnMethods = "searchForItemAndAddToCart")
-    public void proceedToCheckOutPage(){
-        checkOutPage = productPage.goToCheckOut();
-    }
-
-    @Test(dependsOnMethods = "proceedToCheckOutPage")
-    public void fillAndValidateZipCode(){
-        checkOutPage.fillAddress("41110");
-        assertEquals(checkOutPage.cityFieldValue, "Göteborg");
-    }
-
-    @Test(dependsOnMethods = "fillAndValidateZipCode")
-    public void validateIfCheckBoxSelected(){
-        checkOutPage.checkDeliveryOptions();
-        assertTrue(checkOutPage.deliveryFieldMessage.contains("Vänligen välj önskad leveransdag nedan!"));
-    }
-
-    @Test(dependsOnMethods = "validateIfCheckBoxSelected")
-    public void checkForFastestDeliveryDate(){
-        checkOutPage.checkFastestDeliveryOption();
-        assertTrue(!checkOutPage.dateValue.isEmpty());
-        assertEquals(checkOutPage.fastestOptionMessage,"Snabbast möjliga leverans");
-    }
+//    @Test(dependsOnMethods = "searchForItemAndAddToCart")
+//    public void proceedToCheckOutPage(){
+//        checkOutPage = productPage.goToCheckOut();
+//    }
+//
+//    @Test(dependsOnMethods = "proceedToCheckOutPage")
+//    public void fillAndValidateZipCode(){
+//        checkOutPage.fillAddress("41110");
+//        assertEquals(checkOutPage.cityFieldValue, "Göteborg");
+//    }
+//
+//    @Test(dependsOnMethods = "fillAndValidateZipCode")
+//    public void validateIfCheckBoxSelected(){
+//        checkOutPage.checkDeliveryOptions();
+//        assertTrue(checkOutPage.deliveryFieldMessage.contains("Vänligen välj önskad leveransdag nedan!"));
+//    }
+//
+//    @Test(dependsOnMethods = "validateIfCheckBoxSelected")
+//    public void checkForFastestDeliveryDate(){
+//        checkOutPage.checkFastestDeliveryOption();
+//        assertTrue(!checkOutPage.dateValue.isEmpty());
+//        assertEquals(checkOutPage.fastestOptionMessage,"Snabbast möjliga leverans");
+//    }
 }
